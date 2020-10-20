8ad16388d8f88d7732b759dd6d6a299b99bc1bad
machinexa <machinexa@gmail.com>
2020-10-20 20:34:46 +0545

    08:34:46 PM 10/20/2020

--- lib/PathFunctions.py
+++ lib/PathFunctions.py
@@ -1,5 +1,6 @@
 class PathFunction:
     def merge(self, xpath: str, ypath: str) -> str:
+        ''' Merges two parts of url '''
         if not xpath or not ypath:
             return ""
         xpath = self.urlerslasher(xpath) 
@@ -7,12 +8,14 @@ class PathFunction:
         return xpath + ypath
   
     def urlerslasher(self, xpath: str) -> str:
+        ''' Converts a subdomain into a url '''
         if not xpath:
             return xpath
         ypath = self.ender(self.urler(xpath), '/')
         return ypath
     
     def starter(self, xpath:str, ypath: str) -> str:
+        ''' Appends text to start if not found '''
         if not xpath or not ypath:
             return ""
         if xpath[0] != ypath:
@@ -21,6 +24,7 @@ class PathFunction:
             return xpath
 
     def unstarter(self, xpath:str, ypath: str) -> str:
+        ''' Removes text from start if found '''
         if not xpath or not ypath:
             return ""
         if xpath[0] == ypath:
@@ -29,6 +33,7 @@ class PathFunction:
             return xpath
 
     def ender(self, xpath: str, ypath: str) -> str:
+        ''' Appends text to end if not found '''
         if not xpath or not ypath:
             return ""
         if xpath[-1] != ypath:
@@ -37,6 +42,7 @@ class PathFunction:
             return xpath
 
     def unender(self, xpath: str, ypath: str) -> str:
+        ''' Removes text from end if found '''
         if not xpath or not ypath:
             return ""
         if xpath[-1] == ypath:
@@ -44,6 +50,7 @@ class PathFunction:
         return xpath
    
     def urler(self, xpath: str) -> str:
+        ''' Appends http:// to subdomain/domain '''
         if not xpath:
             return xpath
         if not xpath.startswith('http://') and not xpath.startswith('https://'):
@@ -53,6 +60,7 @@ class PathFunction:
         return ypath
 
     def unurler(self, xpath: str) -> str:
+        ''' Removes scheme from url '''
         if not xpath:
             return xpath
         if not '://' in xpath:
