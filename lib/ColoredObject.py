dcbb14cc66979f47faa1fbcf1bd344887830393b
machinexa <machinexa@gmail.com>
2020-10-20 20:28:58 +0545

    08:28:58 PM 10/20/2020

--- lib/ColoredObject.py
+++ lib/ColoredObject.py
@@ -4,3 +4,4 @@ class Color:
         self.information = '\x1b[33m[!]\x1b[0m'
         self.good = '\x1b[32m[+]\x1b[0m'
         self.bad = '\x1b[31m[-]\x1b[0m'
+        self.other = '\x1b[34m[*]\x1b[0m'
